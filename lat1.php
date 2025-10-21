990c59bf959a99fae0413e8a1332b9494dd31478
trisetyabudi2017 <110437412+trisetyabudi2017@users.noreply.github.com>
2025-10-21 20:54:12 +0700

    versi 1.03
    Fungsi If

--- lat1.php
+++ lat1.php
@@ -15,18 +15,52 @@
         // echo'<br>';
         // var_dump($angka);
 
-    function tulisan(){
-        echo 'Saya Belajar Php';
+    // function tulisan(){
+    //     echo 'Saya Belajar Php';
 
-    }
-    tulisan();
-    echo '<br>';
+    // }
+    // tulisan();
+    // echo '<br>';
 
-    function luaspersegi($p=5,$l=10){
+    // function luaspersegi($p=5,$l=10){
        
-        echo $luas=$p*$l;
-    }
-    luaspersegi(10,4);
+    //     echo $luas=$p*$l;
+    // }
+    // luaspersegi(10,4);
+
+    // Fungsi IF
+                $nilai= 60;
+                
+                if ($nilai>=70) {
+                    echo 'Anda Lulus';
+                } else {
+                    echo 'Anda Tidak Lulus';
+                }
+echo '<br>';
+                if ($nilai> 0 && $nilai<=100) {
+                    if ($nilai>=90) {
+                        echo 'A';
+                        }  
+                    else if ($nilai>=80 ){
+                        echo 'B';
+                        } 
+                     else if ($nilai>=70 ){
+                        echo 'C';
+                        }  
+                     else if ($nilai>=60 ){
+                        echo 'D';
+                        }   
+                    else {
+                        echo 'E';
+                        }
+
+                   
+                }
+                else {
+                    echo 'Nilai Anda Tidak Valid';
+                }
+                
+                
 
 
 ?>

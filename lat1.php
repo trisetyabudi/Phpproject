210a226a56b87b6cfff968c48b3f7fe0e0aac949
trisetyabudi2017 <110437412+trisetyabudi2017@users.noreply.github.com>
2025-10-21 15:18:17 +0700

    versi 1.0
    Menampilkan tulisan

---
+++ lat1.php
@@ -0,0 +1,15 @@
+<?php 
+    echo "Belajar php";
+    echo '<br>';
+    echo'Saya Siswa SMK';
+
+    echo'<h1 style="background-color:red">Belajar Php Itu mudah</h1>';
+
+    echo'Saya belajar php'.'PHP'.'<br>';
+
+    echo 'Tahun 2019';
+
+
+
+
+?>
